8d1b56dcd8df0ed5a5fcbdb1e296b03ab251dfdb
JFpro160 <jose.paca@utec.edu.pe>
2024-09-14 19:38:09 -0500

    por fin funciona el cdk en python

--- cdk-python/app.py
+++ cdk-python/app.py
@@ -12,9 +12,11 @@ app = App()
 # Definir el entorno con el Account ID y la región obtenidos automáticamente
 env = Environment(account=account_id, region=region)
 
-# Configurar el sintetizador con el rol LabRole
+# Configurar el sintetizador con el qualifier correcto y el nombre del bucket correcto
 sintetizador = DefaultStackSynthesizer(
-    cloud_formation_execution_role=f"arn:aws:iam::{account_id}:role/LabRole"
+    qualifier="cdk-dep",  # Asegúrate de usar el mismo qualifier que en el bootstrap
+    cloud_formation_execution_role=f"arn:aws:iam::{account_id}:role/LabRole",
+    file_assets_bucket_name=f"cdk-cdk-dep-assets-{account_id}-{region}"  # Ajusta el nombre del bucket
 )
 
 # Crear el stack y pasar el sintetizador con LabRole
